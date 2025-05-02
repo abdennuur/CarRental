96349d7fed20f7c4dab8f6c78c32354f48b35205
abdennuur <abdnourkhatib@gmail.com>
2025-05-02 17:58:08 +0100

    c

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3 col-md-2">
-          <div class="logo"> <a href="index.php"><img src="assets/images/logg.png" alt="image"/></a> </div>
+          <div class="logo"> <a href="index.php"><img src="assets/images/logg.png" alt="image" style="width: 153px; height: 43px;"/></a> </div>
         </div>
         <div class="col-sm-9 col-md-10">
           <div class="header_info">
